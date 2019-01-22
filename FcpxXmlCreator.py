324fc66a0d0cbd689bf3c9e56709182f0aa552cc
nicolas.seibert <zeibou@gmail.com>
2019-01-22 00:09:55 -0500

    big refactor + add new methods skeletons

--- FcpxXmlCreator.py
+++ FcpxXmlCreator.py
@@ -2,10 +2,24 @@
 import xml.etree.ElementTree as ET
 import os
 from pyHiLightExtractor import MultiFilesReader
+from pyHiLightExtractor.MultiFilesReader import HiLightDescriptor
+from pyHiLightExtractor.MultiFilesReader import VideoDescriptor
 import datetime
+from dataclasses import dataclass
+from datetime import timedelta
 
 
-def add_mpeg_asset(root, filepath):
+@dataclass
+class HilightMultiClick:
+    hilight : HiLightDescriptor
+    nb_clicks : int
+
+@dataclass
+class AssetDescriptor:
+    id : str
+    videoDesc : VideoDescriptor
+
+def add_mpeg_asset(root, video_desc):
     res = root.find("resources")
     nb_res = len(res)
     #for e in res:
@@ -13,10 +27,10 @@ def add_mpeg_asset(root, filepath):
     new_asset = ET.Element("asset")
     asset_id = "r"+str(nb_res + 1)
     new_asset.set("id", asset_id)
-    new_asset.set("name", get_asset_name(filepath))
-    new_asset.set("src", "file://" + filepath)
+    new_asset.set("name", get_asset_name(video_desc.name))
+    new_asset.set("src", "file://" + video_desc.path)
     res.append(new_asset)
-    return asset_id
+    return AssetDescriptor(asset_id, video_desc)
 
 
 def build_title_element(title_asset_id, text, offset):
@@ -70,27 +84,54 @@ def open_template(template_xml_file):
     return ET.parse(template_xml_file)
 
 def add_all_mpeg_assets(root, folder, assets):
-    for h in MultiFilesReader.parse_directory(folder, endswith=".mp4"):
-        if not h.path in assets:
-            assets[h.path] = add_mpeg_asset(root, os.path.join(folder, h.path))
+    for h in MultiFilesReader.get_all_videos(folder, endswith=".mp4"):
+        if not h.name in assets:
+            assets[h.name] = add_mpeg_asset(root, h)
     return assets
 
-def add_all_clips(spine, folder, assets, date, timeSeconds):
-    for i, h in enumerate(MultiFilesReader.parse_directory(folder, endswith=".mp4")):
-        clip = ET.Element("asset-clip")
-        clip.set("name", get_asset_name(h.path))
-        clip.set("ref", assets[h.path])
-        start = f"{h.local_time.seconds - timeSeconds}s"
-        clip.set("start", start)
-        clip.set("duration", f"{timeSeconds}s")
+def add_all_clips(spine, folder, assets, title_text, timeSeconds):
+    for i, h in enumerate(detect_multi_clicks(MultiFilesReader.get_all_hilights(folder, endswith=".mp4"), nb_seconds_interval = 2)):
+        clip = add_clip(spine, assets, h, 10, 0)
+
+        # add title
         if i == 0:
-            d = datetime.datetime.strptime(date, "%Y-%m-%d")
             start = f"{h.local_time.seconds - timeSeconds + 3}s"
-            clip.append(build_title_element("r2", d.strftime("%B %d, %Y"), start))
+            clip.append(build_title_element("r2", title_text, start))
+
+
+def add_clip(spine, assets, hilight, duration_seconds, end_offset_seconds):
+    clip = ET.Element("asset-clip")
+    clip.set("name", get_asset_name(hilight.name))
+    clip.set("ref", assets[hilight.name].id)
+    start_seconds = hilight.local_time.seconds - duration_seconds - end_offset_seconds
+    if start_seconds >= 0:
+        clip.set("start", f"{start_seconds}s")
+        clip.set("duration", f"{duration_seconds}s")
         spine.append(clip)
+    else:
+        # adding end of the first clip
+        # todo if hilight is early on a video, we need to insert the end of the previous video
+
+        # adding beginning of the second clip
+        clip.set("start", "0s")
+        clip.set("duration", f"{duration_seconds + start_seconds}s")
+        spine.append(clip)
+    return clip
+
+
+def detect_multi_clicks(hilights, nb_seconds_interval=2):
+    # todo loop reverse to keep first click of multi clicks
+    # output detected multi clicks + discarded extra clicks
+    # return filtered hilights with number of clicks associated (HilightMultiClick)
+    for h in hilights:
+        yield h
+
+
 
 if __name__ == '__main__':
     date = "2019-01-14"
+    d = datetime.datetime.strptime(date, "%Y-%m-%d")
+    title_text = d.strftime("%B %d, %Y")
 
     template_path = "/Users/nicolas.seibert/Documents/foot/2019-01-14/template.fcpxml"
     tree = open_template(template_path)
@@ -99,7 +140,7 @@ if __name__ == '__main__':
     add_all_mpeg_assets(tree.getroot(), folder, assets)
 
     spine = tree.getroot().find("library").find("event").find("project").find("sequence").find("spine")
-    add_all_clips(spine, folder, assets, date, 10)
+    add_all_clips(spine, folder, assets, title_text, 10)
     #<asset-clip name="GH030065" ref="r5" duration="20s" start="10s" format="r3">
 
     for a in assets: print(a, assets[a])
